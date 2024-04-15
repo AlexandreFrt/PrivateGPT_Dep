4b033e7c5b036b264819060e6e24d366be97d03b
Alexandre <foretalexandre37@gmail.com>
2024-04-15 07:57:04 +0200

    init

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+apt -y install wget curl
+
+# Install Wi-Fi drivers
+read -p "Voulez-vous installer les drivers Wi-Fi ? (y/n) : " wifi
+if [ $wifi == "y" ]
+then
+	lspci -nnkd ::280
+	apt -y install firmware-iwlwifi
+fi
+
+# Install cuda
+read -p "Voulez-vous installer les drivers cuda ? (y/n) : " cuda
+if [ $cuda == "y" ]
+then
+	wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-debian11-11-8-local_11.8.0-520.61.05-1_amd64.deb
+	dpkg -i cuda-repo-debian11-11-8-local_11.8.0-520.61.05-1_amd64.deb
+	cp /var/cuda-repo-debian11-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+	add-apt-repository contrib
+	apt update
+	apt -y install cuda
+fi
+
+# Install git
+read -p "Voulez-vous installer git ? (y/n) : " git
+if [ $git == "y" ]
+then
+	apt -y install git
+fi
+
+# Install python3.11
+read -p "Voulez-vous installer python3.11 ? (y/n) : " python
+if [ $python == "y" ]
+then
+	apt -y install software-properties-common build-essential libnss3-dev zlib1g-dev libgdbm-dev libncurses5-dev libssl-dev libffi-dev libreadline-dev libsqlite3-dev libbz2-dev lzma liblzma-dev python-tk python3-tk tk-dev
+	wget https://www.python.org/ftp/python/3.11.7/Python-3.11.7.tgz
+	tar xvf Python-3.11.7.tgz
+	cd Python-3.11.7
+	./configure --enable-optimizations
+	make altinstall
+	python3.11 --version
+	cd ..
+	rm Python-3.11.7.tgz
+	rm -rd Python-3.11.7
+fi
+
+# Install pyenv
+read -p "Voulez-vous installer pyenv ? (y/n) : " pyenv
+if [ $pyenv == "y" ]
+then
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+	exec "$SHELL"
+fi
+
+# Install poetry
+read -p "Voulez-vous installer poetry ? (y/n) : " poetry
+if [ $poetry == "y" ]
+then
+	curl -sSL https://install.python-poetry.org | python3 -
+	export PATH="$HOME/.local/bin:$PATH"
+	exec "$SHELL"
+fi
