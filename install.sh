9db8597e09d2957bdefb2b572ce299a8e36694f1
Alexandre <foretalexandre37@gmail.com>
2024-04-29 15:43:45 +0200

    fix: Fix /usr/sbin path in root bashrc

--- install.sh
+++ install.sh
@@ -61,6 +61,7 @@ then
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 	echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+	echo 'export PATH="$PATH:/usr/sbin"' >> ~/.bashrc
 	exec "$SHELL"
 fi
 
